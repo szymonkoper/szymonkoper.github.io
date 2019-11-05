2ff0170e64c1e68970de9176facdfb9283374a4f
Szymon Koper <sz.koper@gmail.com>
2019-11-05 16:28:20 +0100

    Adds .eslintrc.js and .prettierrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  plugins: [
+    'better-styled-components',
+    'import',
+    'jest',
+    'jsx-a11y',
+    'prettier',
+    'react-hooks',
+    'react'
+  ],
+  rules: {
+    'better-styled-components/sort-declarations-alphabetically': 'warn',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index'
+        ]
+      }
+    ],
+    'no-return-assign': ['error', 'except-parens'],
+    'no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', ignoreRestSiblings: true }
+    ],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
+    'semi': ['error', 'never']
+  }
+}
